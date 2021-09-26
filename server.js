0dc2b962ac2da59b4cf8add6683721a353dca856
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-26 12:47:35 -0500

    Prueba

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path')
 
 const app =  express();
 
-app.use(express.static(__dirname+'/dist/ionic'))
+app.use(express.static(__dirname+'/dist'))
 app.get('/',(req,res)=>
     {
-        res.sendFile(path.join(__dirname+'/dist/ionic/index.html'));
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
     }
 );
 
