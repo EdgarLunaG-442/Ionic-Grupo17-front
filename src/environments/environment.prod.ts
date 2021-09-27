eb9f67d33059e20f102a119214b19f9e295efa56
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-27 12:54:26 -0500

    con ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: `https://ec2-18-221-74-226.us-east-2.compute.amazonaws.com`
+  baseUrl: `18.221.74.226`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: `https://ec2-18-221-74-226.us-east-2.compute.amazonaws.com`
+  baseUrl: `18.221.74.226`
 };
 
 /*
